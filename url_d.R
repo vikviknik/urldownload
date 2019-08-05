96e7790bf8fd1f914dc1006e509c04adb9d1a7ce
vikviknik <36329982+vikviknik@users.noreply.github.com>
2019-08-05 15:47:31 +0300

    Update url_d.R

--- url_d.R
+++ url_d.R
@@ -5,27 +5,27 @@ library(xml2)
 
 #download files of 2014-2019 years
 
-url <- "https://www.cbr.ru/publ/vestnik/year/2014/"
+url <- "https://www.my_url"
 html <- paste(readLines(url), collapse="\n")
 matched <- str_match_all(html, "<a target=\"_blank\" href=\"(.*?)\">")
 m14 <- matched[[1]][, 2][-103]
-url <- "https://www.cbr.ru/publ/vestnik/year/2015/"
+url <- "https://www.my_url"
 html <- paste(readLines(url), collapse="\n")
 matched <- str_match_all(html, "<a target=\"_blank\" href=\"(.*?)\">")
 m15 <- matched[[1]][, 2][-c(112:118)]
-url <- "https://www.cbr.ru/publ/vestnik/year/2016/"
+url <- "https://www.my_url"
 html <- paste(readLines(url), collapse="\n")
 matched <- str_match_all(html, "<a target=\"_blank\" href=\"(.*?)\">")
 m16 <- matched[[1]][, 2][-c(105:109)]
-url <- "https://www.cbr.ru/publ/vestnik/year/2017/"
+url <- "https://www.my_url"
 html <- paste(readLines(url), collapse="\n")
 matched <- str_match_all(html, "<a target=\"_blank\" href=\"(.*?)\">")
 m17 <- matched[[1]][, 2][-99]
-url <- "https://www.cbr.ru/publ/vestnik/year/2018/"
+url <- "https://www.my_url"
 html <- paste(readLines(url), collapse="\n")
 matched <- str_match_all(html, "<a target=\"_blank\" href=\"(.*?)\">")
 m18 <- matched[[1]][, 2][-87]
-url <- "https://www.cbr.ru/publ/vestnik/year/2019/"
+url <- "https://www.my_url"
 html <- paste(readLines(url), collapse="\n")
 matched <- str_match_all(html, "<a target=\"_blank\" href=\"(.*?)\">")
 m19 <- matched[[1]][, 2][-39]
@@ -42,7 +42,7 @@ dat <- c(1:539)
 for(i in 1:length(M)) {
   dat[i] = M[i]}
 for (j in 1:539) {
-  download.file(url = paste("https://www.cbr.ru", dat[j], sep = ""), destfile = paste("./vestnik",names[j] , ".pdf", sep = ""), mode = "wb")
+  download.file(url = paste("https://my_url", dat[j], sep = ""), destfile = paste("./vestnik",names[j] , ".pdf", sep = ""), mode = "wb")
 }
 
 #join pff files in one with pdftk
@@ -50,7 +50,3 @@ for (j in 1:539) {
 infiles <- Sys.glob("vestnik*.pdf") 
 outfile <- "vestnik.pdf" 
 system(paste("pdftk", paste(infiles, collapse = " "), "cat output", outfile))
-
-
-#https://www.cbr.ru/Queries/XsltBlock/File/48351?fileId=-1&amp;scope=1595-1596
-#https://www.cbr.ru/Queries/XsltBlock/File/48351?fileId=-1&scope=1595-1596
